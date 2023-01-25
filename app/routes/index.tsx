216b20139b5e67fcd382eb27589b607f7685ff09
Emil Bryggare <emil@emilbryggare.com>
2023-01-25 11:44:03 +0000

    fix

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,35 +2,19 @@ import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
-const getData = async () => {
-  return { message: "Hello World!" };
-};
-
 export const loader = async ({ request }: LoaderArgs) => {
-  let data = await getData();
   // Generate correct types.
-  // if (data) {
-  //   return json({ myData: { ifMessage: data } });
-  // }
-  // return json({ myData: { endMessage: data } });
+  // return json({ data: { message: "Hello World!" } });
 
-  // Does not generate correct types.
-  if (data) {
-    return json({ data: { ifMessage: data } });
-  }
-  return json({ data: { endMessage: data } });
+  // Does not generate correct types, e.g. SerializeDeferred
+  return json({ data: { message: "Hello World!" } });
 };
 export default function Index() {
   let loaderData = useLoaderData<typeof loader>();
   // Type is when data key is present in return object:
-  // SerializeDeferred<{
-  //     ifMessage: {
-  //         message: string;
-  //     };
-  // }> | SerializeDeferred<{
-  //     endMessage: never;
+  //   SerializeDeferred<{
+  //     message: string;
   // }>
-
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
